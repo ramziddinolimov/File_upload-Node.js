f4c1a2b8d21f8694e0dfcecd1c42bac279d73042
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-08-24 22:14:26 +0500

    index ejs started

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-const expess = require('express');
+const express = require('express');
 const multer = require('multer');
 const ejs = require('ejs');
 const path = require('path');
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + './public'));
+
+app.get('/', (req, res) => res.render('index'));
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
